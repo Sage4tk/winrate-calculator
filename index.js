b4e0b737254b61662dcbd86176900cd7c4f44004
timothy timbol <ttimbol08@gmail.com>
2022-01-23 20:37:33 +0400

    started a new formula to calculate more than 50 percent

--- index.js
+++ index.js
@@ -9,10 +9,29 @@ const calculateWins = (totalMatches, currentWinRate, wantedWinRate = 50) => {
         }
     }
 
-    //calculate loss and win
-    const wins = parseFloat(((currentWinRate / 100) * totalMatches).toFixed(2));
-    const loses = totalMatches - wins;
+    //variables
+    let wins = parseFloat(((currentWinRate / 100) * totalMatches).toFixed(2));
+    let loses = totalMatches - wins;
+    const winStatic = wins;
 
+    //if wanted winrate is not equal to 50%
+    if (wantedWinRate !== 50) {
+        let neededWins = 0;
+        while (wins / totalMatches < wantedWinRate / 100) {
+            wins++
+            neededWins++;
+        }
+
+
+        return {
+            message: `You need ${neededWins} wins to have a ${wantedWinRate}% win rate.`,
+            numberTW: neededWins,
+            wins: winStatic,
+            loses: loses
+        }
+    } 
+
+    //calculate loss and win for 50%
     return {
         message: `You need ${parseFloat((loses - wins).toFixed(2))} to have a ${wantedWinRate}% win rate`,
         numberTW: parseFloat((loses - wins).toFixed(2)),
@@ -20,5 +39,3 @@ const calculateWins = (totalMatches, currentWinRate, wantedWinRate = 50) => {
         loses: loses
     };
 }
-
-console.log(calculateWins(152, 40))
